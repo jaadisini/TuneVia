6ad352ee54998443a4b16efa53f6c427df837fed
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-23 00:59:32 +0800

    Update start.py

--- Tune/utils/inline/start.py
+++ Tune/utils/inline/start.py
@@ -11,15 +11,15 @@ def private_panel(_):
     return [
         [
             InlineKeyboardButton(
-                text=_["S_B_1"],
+                text=_["S_B_5"],
                 url=f"https://t.me/{app.username}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(text=_["S_B_7"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_4"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_3"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_3"], callback_data="open_help"),
+            InlineKeyboardButton(text=_["S_B_1"], callback_data="open_help"),
         ],
     ]
