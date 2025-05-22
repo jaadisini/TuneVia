3385132afdfe9d868845ab031e1b2bf44a7e01ec
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-23 01:01:32 +0800

    Update config.py

--- config.py
+++ config.py
@@ -103,7 +103,7 @@ DURATION_LIMIT = time_to_seconds(f"{DURATION_LIMIT_MIN}:00")
 
 
 # ───── Bot Introduction Messages ───── #
-AYU = ["💞", "🦋", "🔍", "🧪", "⚡️", "🔥", "🎩", "🌈", "🍷", "🥂", "🥃", "🕊️", "🪄", "💌", "🧨"]
+AYU = ["🔍", "⚡️", "🔥"]
 
 
 # ───── Runtime Structures ───── #
