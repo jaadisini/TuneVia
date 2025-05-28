528dea097fd454700f129e2d120d39f403f09e50
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-05-28 15:22:03 +0800

    Update ping.py

--- Tune/plugins/tools/ping.py
+++ Tune/plugins/tools/ping.py
@@ -7,7 +7,7 @@ from Tune import app
 from Tune.core.call import JARVIS
 from Tune.utils import bot_sys_stats
 from Tune.utils.decorators.language import language
-from Tune.utils.inline import supp_markup
+from Tune.utils.inline import help_keyboard
 from config import BANNED_USERS, PING_VID_URL
 
 
@@ -24,5 +24,5 @@ async def ping_com(client, message: Message, _):
     resp = (datetime.now() - start).microseconds / 1000
     await response.edit_text(
         _["ping_2"].format(resp, app.mention, UP, RAM, CPU, DISK, pytgping),
-        reply_markup=supp_markup(_),
+        reply_markup=help_keyboard(_),
     )
