4f1ab0c83f9bf3ce865478930d55d4e7d840ad85
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-06-01 09:41:53 +0800

    Update help.py

--- Tune/utils/inline/help.py
+++ Tune/utils/inline/help.py
@@ -3,29 +3,31 @@ from Tune import app
 
 def help_keyboard(_):
     buttons = []
-    for i in range(1, 16):
+    for i in range(1, 18):  # Dari H_B_1 sampai H_B_17
         if (i - 1) % 3 == 0:
-            buttons.append([])
+            buttons.append([])  # Baris baru setiap 3 tombol
         buttons[-1].append(
             InlineKeyboardButton(
                 text=_[f"H_B_{i}"],
                 callback_data=f"help_callback hb{i}"
             )
         )
+    
+    # Tambahkan baris terakhir untuk MENU & CLOSE
     buttons.append(
         [
             InlineKeyboardButton(
                 text="ᴍᴇɴᴜ", 
-                callback_data="back_to_main"),
-                
+                callback_data="back_to_main"
+            ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"],
                 callback_data="close"
             )
         ]
     )
-    return InlineKeyboardMarkup(buttons)
 
+    return InlineKeyboardMarkup(buttons)
 
 
 def help_back_markup(_):
@@ -50,7 +52,7 @@ def private_help_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url="https://t.me/{0}?start=help".format(app.username)
+                url=f"https://t.me/{app.username}?start=help"
             )
         ]
     ]
