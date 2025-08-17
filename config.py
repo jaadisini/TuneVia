1e55b5a0d35394a3d7959f8c63b3ce1be57fa7b9
jaadisini <162589971+jaadisini@users.noreply.github.com>
2025-08-17 13:42:44 +0800

    Update config.py

--- config.py
+++ config.py
@@ -71,26 +71,26 @@ PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "2500"))
 
 # ───── Bot Media Assets ───── #
 START_VIDS = [
-    "https://files.catbox.moe/1zpysu.jpg",
+    "https://files.catbox.moe/s5fey0.jpg",
 ]
 
 STICKERS = [
-    "CAACAgUAAx0Cd6nKUAACASBl_rnalOle6g7qS-ry-aZ1ZpVEnwACgg8AAizLEFfI5wfykoCR4h4E",
-    "CAACAgUAAx0Cd6nKUAACATJl_rsEJOsaaPSYGhU7bo7iEwL8AAPMDgACu2PYV8Vb8aT4_HUPHgQ"
+    "CAACAgUAAyEGAASjG7MBAAIeRmiha5UzWPvDGgh90t2fsaeb8s60AAIIGQACi7rJVHQD5s5RcEN9HgQ",
+    "CAACAgUAAyEGAASjG7MBAAIeSWiha7I3ZT-QlxUC9oaMrrAucBDVAAJeFwACrjjBVBTExokRLfQOHgQ"
 ]
-HELP_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-PING_VID_URL = "https://files.catbox.moe/1zpysu.jpg"
-PLAYLIST_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-STATS_VID_URL = "https://files.catbox.moe/1zpysu.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/1zpysu.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/1zpysu.jpg"
+HELP_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+PING_VID_URL = "https://files.catbox.moe/s5fey0.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+STATS_VID_URL = "https://files.catbox.moe/s5fey0.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/s5fey0.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/s5fey0.jpg"
 STREAM_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/1zpysu.jpg"
-FAILED = "https://files.catbox.moe/a2d9ok.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/s5fey0.jpg"
+FAILED = "https://files.catbox.moe/s5fey0.jpg"
 
 # ───── Utility & Functional ───── #
 def time_to_seconds(time: str) -> int:
